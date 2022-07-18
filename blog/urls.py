38e4d67f4ad27a9f5502471d99abd46578bfa3c0
sookbun <blackdew7@gmail.com>
2022-07-18 05:25:28 +0000

    add class view sample code

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,16 @@
 from django.http import HttpRequest, HttpResponse
 from django.urls import include, path
-
 from . import views
 
 urlpatterns = [
     path('', views.index),
     path('post_list', views.post_list)
 ]
+
+# from django.views.generic import ListView
+# from . import models
+
+# urlpatterns = [
+#     path('', views.index),
+#     path('post_list', ListView.as_view(model=models.Post))
+# ]
