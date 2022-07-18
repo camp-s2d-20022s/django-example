fdc200ee04372ebb9615d4bfa731abb0eac231b2
sookbun <blackdew7@gmail.com>
2022-07-18 04:51:40 +0000

    use post model in view

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import models
 
 def index(req):
     return HttpResponse('blog')
 
 def post_list(req):
-    posts = [
-        {'pk': 1, 'title': 'html', 'content': 'html is ...'},
-        {'pk': 2, 'title': 'css', 'content': 'css is ...'},
-        {'pk': 3, 'title': 'javascipt', 'content': 'javascipt is ...'},
-        {'pk': 4, 'title': 'python', 'content': 'python is ...'},
-        {'pk': 5, 'title': 'django', 'content': 'django is ...'},
-    ]
-
+    posts = models.Post.objects.all()
     return render(req, 'blog/post_list.html', {"post_list": posts})
