75efa088ae436e3499c15f8999bda6ad195d50d8
sookbun <blackdew7@gmail.com>
2022-07-18 06:51:26 +0000

    string 변환

--- toy/views.py
+++ toy/views.py
@@ -11,10 +11,10 @@ def gugu(req, num):
 
     # list comprehension
     mul_table = [f"{num} * {i} = {num * i}" for i in range(1, 10)]
-    print(2, mul_table)
+    print(2, "<br>".join(mul_table))
 
     # map + functinal
     mul_table = list(map(lambda i: f"{num} * {i} = {num * i}", range(1, 10)))
-    print(3, mul_table)
+    print(3, "\n".join(mul_table))
 
     return render(req, 'toy/gugu.html', {'mul_table': mul_table})
