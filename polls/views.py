b0ece864eba0ada19048897089329bb2f74e3448
sookbun <blackdew7@gmail.com>
2022-07-19 04:47:47 +0000

    views 함수 추가

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def index(req):
     return HttpResponse("polls")
+
+def detail(req, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(req, question_id):
+    response = "You're looking at the results of question {0}. {1} {0}"
+    return HttpResponse(response.format(question_id, "aaa"))
+
+def vote(req, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
