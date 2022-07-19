d616d29d16a85cf305deaea0a28fb8c955efe994
sookbun <blackdew7@gmail.com>
2022-07-19 05:41:47 +0000

    detail page

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from . import models
@@ -8,7 +8,14 @@ def index(req:HttpRequest) -> HttpResponse:
     return render(req, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 def detail(req, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    # try:
+    #     question = models.Question.objects.get(pk=question_id)
+    # except models.Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = models.Question.objects.filter(pk=question_id)
+    if len(question) == 0:
+        raise Http404("Question does not exist")
+    return render(req, 'polls/detail.html', {'question': question[0]})
 
 def results(req, question_id):
     response = "You're looking at the results of question {0}. {1} {0}"
