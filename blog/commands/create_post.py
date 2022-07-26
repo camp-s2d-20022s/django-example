bc0d05f8172a159f273c7729d326de31681f57a6
sookbun <blackdew7@gmail.com>
2022-07-26 02:58:09 +0000

    blog post 생성하는 명령으로 조정

--- blog/commands/create_post.py
+++ blog/commands/create_post.py
@@ -1,16 +1,16 @@
 from django.core.management.base import BaseCommand, CommandError
-from polls import models
+from blog import models
 
 class Command(BaseCommand):
-    help = 'Create Question'
+    help = 'Create Post'
 
     def add_arguments(self, parser):
         parser.add_argument('title', type=str)
         parser.add_argument('content', type=str)
 
     def handle(self, *args, **options):
-        q = models.Question(title=options['title'],
-                            content=options['content'])
-        q.save()
+        p = models.Post(title=options['title'],
+                        content=options['content'])
+        p.save()
 
-        self.stdout.write(self.style.SUCCESS(f'Successfully closed Question "{q.id}"'))
+        self.stdout.write(self.style.SUCCESS(f'Successfully Created Post "{p.id}"'))
