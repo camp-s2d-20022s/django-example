8e26956aa1b5b6e85a4bad7af90e449381744e37
sookbun <blackdew7@gmail.com>
2022-08-01 01:56:02 +0000

    api/snippets view & router 연결

--- mysite/urls.py
+++ mysite/urls.py
@@ -48,5 +48,6 @@ router.register(r'users', UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
