64ea24cc0b41ae989cd9c62c359ad044c305e937
sookbun <blackdew7@gmail.com>
2022-08-01 02:31:44 +0000

    use format suffix

--- api/views.py
+++ api/views.py
@@ -6,28 +6,28 @@ from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api.models import Snippet
-from api.serializers import SnippetSerializer
+from api.serializers import SnippetSerializer, SnippetModelSerializer
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
         snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
+        serializer = SnippetModelSerializer(snippets, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetModelSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
     """
@@ -37,12 +37,12 @@ def snippet_detail(request, pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
+        serializer = SnippetModelSerializer(snippet)
         return Response(serializer.data)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetModelSerializer(snippet, data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
